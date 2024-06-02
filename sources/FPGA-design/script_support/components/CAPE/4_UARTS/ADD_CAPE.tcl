c216a5e6ec02b415e69e4a9a01256be8ff5a233e
vauban353 <vauban353@gmail.com>
2024-06-02 09:53:00 +0100

    Cape: 4 UARTS: Correct SPI pin constraints.

--- sources/FPGA-design/script_support/components/CAPE/4_UARTS/ADD_CAPE.tcl
+++ sources/FPGA-design/script_support/components/CAPE/4_UARTS/ADD_CAPE.tcl
@@ -31,6 +31,8 @@ sd_create_scalar_port -sd_name ${sd_name} -port_name {P9_PIN14} -port_direction 
 sd_create_scalar_port -sd_name ${sd_name} -port_name {P9_PIN16} -port_direction {OUT}
 sd_create_scalar_port -sd_name ${sd_name} -port_name {P9_PIN42} -port_direction {OUT}
 
+sd_create_scalar_port -sd_name ${sd_name} -port_name {P9_29} -port_direction {IN} 
+
 #-------------------------------------------------------------------------------
 # Instantiate.
 #-------------------------------------------------------------------------------
@@ -96,13 +98,12 @@ sd_rename_port -sd_name ${sd_name} -current_port_name {MMUART_3_RXD} -new_port_n
 
 sd_clear_pin_attributes -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:SPI_1_SS1} 
 sd_clear_pin_attributes -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:SPI_1_CLK} 
-sd_clear_pin_attributes -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:SPI_1_DO} 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:SPI_1_DO} -port_name {} 
+sd_clear_pin_attributes -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:SPI_1_DI} 
 sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:SPI_1_SS1} -port_name {} 
 sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:SPI_1_CLK} -port_name {} 
 sd_rename_port -sd_name ${sd_name} -current_port_name {SPI_1_SS1} -new_port_name {P9_28} 
 sd_rename_port -sd_name ${sd_name} -current_port_name {SPI_1_CLK} -new_port_name {P9_31} 
-sd_rename_port -sd_name ${sd_name} -current_port_name {SPI_1_DO} -new_port_name {P9_29} 
+sd_connect_pins -sd_name ${sd_name} -pin_names {"BVF_RISCV_SUBSYSTEM:SPI_1_DI" "P9_29"}
 
 sd_connect_pins_to_constant -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:M2_UART_CTS} -value {VCC} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:M2_UART_RTS} 
